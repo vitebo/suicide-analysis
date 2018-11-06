9d8d7b7ee686e442500830c4a221631ff2e08008
vitebo <vitebo51@gmail.com>
2018-11-05 22:24:32 -0200

    Iniciando uma documentação básica para o serviço

--- src/core/__main__.py
+++ src/core/__main__.py
@@ -1,3 +1,6 @@
+from more_itertools import seekable
+
 from src.core.suicide_data_service import SuicideDataService
 
 service = SuicideDataService({'Arquivos': ['obtsp11.dbf', 'obtsp05.dbf', 'obtsp00.dbf']})
+print(service.search())

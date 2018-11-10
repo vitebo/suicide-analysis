1d4f3bc887b5b14c22fc5fc50816e4e4c9926ee7
vitebo <vitebo51@gmail.com>
2018-11-10 06:55:51 -0200

    separando os filtros

--- src/services/mortality_data_service/model.py
+++ src/services/mortality_data_service/model.py
@@ -2,6 +2,7 @@ from io import StringIO
 
 import requests
 from bs4 import BeautifulSoup
+from filters import get_filters_default
 
 
 class MortalityDataService:
@@ -12,32 +13,7 @@ class MortalityDataService:
         self.__HEADERS = {
             'user-agent': 'my-app'
         }
-        self.__filters_default = {
-            'Linha': 'Município',
-            'Coluna': '--Não-Ativa--',
-            'Incremento': 'Óbitos_p/Residênc',
-            'Arquivos': 'obtsp16.dbf',
-            'SMunicípio': 'TODAS_AS_CATEGORIAS__',
-            'SRegião_de_Saúde_(CIR)': 'TODAS_AS_CATEGORIAS__',
-            'SMacrorregião_de_Saúde': 'TODAS_AS_CATEGORIAS__',
-            'SDivisão_administ_estadual': 'TODAS_AS_CATEGORIAS__',
-            'SMicrorregião_IBGE': 'TODAS_AS_CATEGORIAS__',
-            'SRegião_Metropolitana_-_RIDE': 'TODAS_AS_CATEGORIAS__',
-            'SCapítulo_CID-10': 'TODAS_AS_CATEGORIAS__',
-            'SGrupo_CID-10': 'TODAS_AS_CATEGORIAS__',
-            'SCausa_-_CID-BR-10': 'TODAS_AS_CATEGORIAS__',
-            'SCausa_mal_definidas': 'TODAS_AS_CATEGORIAS__',
-            'SFaixa_Etária': 'TODAS_AS_CATEGORIAS__',
-            'SFaixa_Etária_OPS': 'TODAS_AS_CATEGORIAS__',
-            'SFaixa_Etária_det': 'TODAS_AS_CATEGORIAS__',
-            'SFx.Etária_Menor_1A': 'TODAS_AS_CATEGORIAS__',
-            'SSexo': 'TODAS_AS_CATEGORIAS__',
-            'SCor/raça': 'TODAS_AS_CATEGORIAS__',
-            'SEscolaridade': 'TODAS_AS_CATEGORIAS__',
-            'SEstado_civil': 'TODAS_AS_CATEGORIAS__',
-            'SLocal_ocorrência': 'TODAS_AS_CATEGORIAS__',
-            'formato': 'prn'
-        }
+        self.__filters_default = get_filters_default()
 
     def search(self, options=None):
         data = self.__create_data(options)
